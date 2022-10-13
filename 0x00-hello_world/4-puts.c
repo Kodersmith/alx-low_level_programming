f179a45812095dd44ea5527d747cc233796b0f00
Kodersmith <adeoyeadesanya88@gmail.com>
2022-10-13 13:46:13 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-
-/**
- *main - is the entry point
- *
- * return: by default returns zero
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 
