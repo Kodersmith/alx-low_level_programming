d2e643c93c3e55e5f4e4076d08a3f05900637791
Kodersmith <adeoyeadesanya88@gmail.com>
2022-10-16 13:09:23 -0700

    101__quo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 /**
- * main-entry point
+ *main-entry point
  *
- * Return: 1 always
+ *Return: 1 always
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\n-Dora Korpar, 2015-10-19\n", 59);
-	Return(1);
+	return (1);
 }
 
