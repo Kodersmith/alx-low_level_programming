aab4dbb7c115026f86c105c929cf59e7a30a20ab
Kodersmith <adeoyeadesanya88@gmail.com>
2022-10-20 18:56:47 -0700

    upperc

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
  *_isupper-checks if a character is uppercase
- *@c:variable test
- *Return: Always 0
+ *@c:input character
+ *Return: returns 1 if it is an uppercase character, 0 in other case
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 {
 	return (1);
 }
