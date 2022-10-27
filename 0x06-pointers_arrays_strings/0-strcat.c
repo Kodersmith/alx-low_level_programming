fcce629a5c0319b4a321ac4fe50e1193229f4b19
Kodersmith <adeoyeadesanya88@gmail.com>
2022-10-27 15:33:29 -0700

    Taskupdate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,15 +3,14 @@
 /**
  *_strcat-concatenates two strings
  *
- *@dest: destination
- *@src: source
- *Return: the pointer to dest
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
 	while (dest[i] != '\0')
