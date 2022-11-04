232cff150e6c0efb6ad92ebd08ceaf6c08e81034
Kodersmith <adeoyeadesanya88@gmail.com>
2022-11-04 04:27:31 -0700

    2-arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main-print the name of the program
+ *@argc: count argument
+ *@argv: Arguments
+ *Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int count = 0;
+
+	if (argc > 0)
+{
+	/*WHILE - print each arguments*/
+	while (count < argc)
+{
+	printf("%s\n", argv[count]);
+	return (0);
+}
+}
+}
